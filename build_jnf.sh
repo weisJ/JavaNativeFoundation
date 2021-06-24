3ab9c243bc55917aa9ef4c20abd2060de02050ab
Jannis Weis <weisj@arcor.de>
2021-06-24 14:05:22 +0200

    Add script for building JNF only.

---
+++ build_jnf.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+if [[ $(arch) == "arm64" ]] ; then
+    echo "Re-execing build using Rosetta." >&2
+    exec arch -x86_64 ${0} "${@}"
+fi
+
+SDK_NAME=macosx
+
+CONCURRENCY=$(sysctl -n hw.activecpu)
+CODE_SIGN_IDENTITY=${AMFITRUSTED_IDENTITY:--}
+DEBUG_LEVEL=release
+
+# Exporting this such that /usr/bin/clang uses it implictly
+export SDKROOT=$(xcrun --sdk ${SDK_NAME} --show-sdk-path)
+export PATH=/AppleInternal/Java/bin:$PATH
+
+do_jnf() {
+    mkdir -p build/Frameworks
+    xcodebuild install -project openjdk/apple/JavaNativeFoundation/JavaNativeFoundation.xcodeproj -target JavaNativeFoundation -configuration Release DSTROOT="$(pwd)/build/Frameworks" CODE_SIGN_IDENTITY="${CODE_SIGN_IDENTITY}"
+}
+
+do_jnf
